dad97b2198f8e0ab0c43af60b01ecc9204c968c9
ItsTheSanchari <sanchari678@gmail.com>
2021-07-20 21:55:39 +0530

    added cron set up and made it work for every 4 seconds

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cron = require('node-cron')
 const app = express();
 
 app.get('/',(req,res,next)=> {
@@ -15,11 +16,14 @@ app.get('all/jobs',(req,res,next)=> {
 })
 
 app.get('all/failed/jobs',(req,res,next)=> {
-    
+
 })
 
 app.post('/reschedule',(req,res,next)=> {
 
+})
+cron.schedule("*/4 * * * * *",()=> {
+    console.log('running at every 4 second')
 })
 app.listen(3000,()=> {
     console.log('listening on port 3000')
