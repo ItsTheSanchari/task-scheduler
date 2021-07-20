4450721ef8870fd0b1021b2fc320d2ab956dcddc
ItsTheSanchari <sanchari678@gmail.com>
2021-07-20 23:22:10 +0530

    created db connection

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const cron = require('node-cron')
 const mail = require('./jobs/mailSender')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
 const app = express();
 
 app.get('/',(req,res,next)=> {
@@ -26,6 +29,10 @@ app.post('/reschedule',(req,res,next)=> {
 cron.schedule("*/4 * * * * *",()=> {
     mail.sender()
 })
-app.listen(3000,()=> {
-    console.log('listening on port 3000')
+mongoose.connect(process.env.URI,{useNewUrlParser: true, useUnifiedTopology: true}).then(()=> {
+    app.listen(3000,()=> {
+        console.log('listening on port 3000')
+    })
+}).catch((err)=> {
+    console.log('getting error while connecting to db',err)
 })
